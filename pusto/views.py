9c0c91b9c65a981bcf2536c2839d169a6e0e49f2
Grisha aka naspeh <naspeh@gmail.com>
2010-11-05 12:32:12 +0200

    Added redirection to ZF articlei from old url;

--- pusto/views.py
+++ pusto/views.py
@@ -2,3 +2,11 @@ from naya import Module
 
 
 mod = Module(__name__)
+
+
+@mod.route(
+    '/blog/2008/09/25/avtozagruzka-klassov-v-prilozheniyah-na-zend-framework/',
+    redirect_to='post/avtozagruzka-klassov-v-prilozheniyah-na-zend-framework/'
+)
+def redirect(app):
+    pass
