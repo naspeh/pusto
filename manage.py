d917314fc802d74961827cca0e402e3a6468bf76
Grisha aka naspeh <naspeh@gmail.com>
2010-11-07 00:12:35 +0200

    Updated manage.py, removed fabric relation;

--- manage.py
+++ manage.py
@@ -1,6 +1,5 @@
 #!/usr/bin/env python
-from fabric.api import local
-from naya.script import make_shell
+from naya.script import make_shell, sh
 from werkzeug.script import make_runserver, run
 
 from pusto import app
@@ -8,18 +7,18 @@ from pusto import app
 
 def action_pep8(target='.'):
     '''Run pep8.'''
-    local('pep8 --ignore=E202 %s' % target, capture=False)
+    sh('pep8 --ignore=E202 %s' % target)
 
 
 def action_clean(mask=''):
     '''Clean useless files.'''
     masks = [mask] if mask else ['*.pyc', '*.pyo', '*~', '*.orig']
     command = ('find . -name "%s" -exec rm -f {} +' % mask for mask in masks)
-    local('\n'.join(command), capture=False)
+    sh('\n'.join(command))
 
 
 action_shell = make_shell(lambda: {'app': app})
-action_runserver = make_runserver(
+action_run = make_runserver(
     lambda: app, use_reloader=True, use_debugger=True
 )
 
