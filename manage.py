22023523ef4903cd81458793fe2c6aa4b122a849
Grisha aka naspeh <naspeh@gmail.com>
2010-11-07 22:12:37 +0200

    Added 'deploy' target in manage.py;

--- manage.py
+++ manage.py
@@ -5,6 +5,13 @@ from werkzeug.script import make_runserver, run
 from pusto import app
 
 
+SERVER_PARAMS = {
+    'activate': 'source /root/.virtualenvs/pusto/bin/activate && which python',
+    'project_path': '/var/www/nanaya',
+    'naya_path': '/root/repos/naya'
+}
+
+
 def action_pep8(target='.'):
     '''Run pep8.'''
     sh('pep8 --ignore=E202 %s' % target)
@@ -15,6 +22,24 @@ def action_clean(mask=''):
     masks = [mask] if mask else ['*.pyc', '*.pyo', '*~', '*.orig']
     command = ('find . -name "%s" -exec rm -f {} +' % mask for mask in masks)
     sh('\n'.join(command))
+
+
+def action_deploy(host='yadro.org'):
+    '''Deploy code on server.'''
+    if host:
+        sh(('pwd', 'hg push'))
+        sh((
+            '{activate}',
+            '{project_path}/manage.py deploy ""'
+        ), host=host, params=SERVER_PARAMS)
+    return
+
+    sh((
+        '{activate}',
+        'cd {naya_path}', 'pwd', 'hg up', 'pip  install .',
+        'cd {project_path}', 'pwd', 'hg pull', 'hg up',
+    ), params=SERVER_PARAMS)
+    sh(('{project_path}/pusto.fcgi > /dev/null &'), params=SERVER_PARAMS)
 
 
 action_shell = make_shell(lambda: {'app': app})
