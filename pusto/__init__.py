c5d2e21106afbb01657ccf09dab928a2e0dc79ce
Grisha aka naspeh <naspeh@gmail.com>
2010-11-19 23:10:40 +0200

    Implemented base layout for editor;

--- pusto/__init__.py
+++ pusto/__init__.py
@@ -13,7 +13,8 @@ app = App(__name__, {
         'path_ends': ['/index.html', '.html'],
         'path_allow': [
             'index.html', 'naspeh.html', 'resume.html', 'post/*',
-            'googlee71e35f8e9cbd607.html'
+            'googlee71e35f8e9cbd607.html',
+            '*.*'
         ],
         'filters': filters.all_by_name
     }
