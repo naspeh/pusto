854fa61b2be1b48543ae2524396e91e654d76a23
Grisha K <naspeh@gmail.com>
2013-08-22 13:13:56 +0300

    Remove `argh` from dependencies;

--- manage.py
+++ manage.py
@@ -1,27 +1,47 @@
 #!/usr/bin/env python
+import argparse
+import http
 import os
-from http import server
 
-from argh import arg, dispatch_commands
 from werkzeug.serving import run_simple
 
 from pusto import app, data
 
 
-def run(host='localhost', port=5000):
-    '''start dev server'''
-    run_simple(host, port, app, use_reloader=True, use_debugger=True)
+def process_args():
+    parser = argparse.ArgumentParser()
+    subs = parser.add_subparsers()
 
+    def sub(name, **kw):
+        s = subs.add_parser(name, **kw)
+        s.set_defaults(sub=name)
+        s.arg = lambda *a, **kw: s.add_argument(*a, **kw) and s
+        return s
 
-@arg('--serve', '-s', help='run server on `build` directory')
-def build(serve=False):
-    '''build static content from `data` directory'''
-    build_dir = './var/data'
-    data.build('./data', build_dir)
-    if serve:
-        os.chdir(build_dir)
-        server.test(HandlerClass=server.SimpleHTTPRequestHandler)
+    sub('run', help='start dev server')\
+        .arg('--host', default='localhost')\
+        .arg('--port', type=int, default=5000)
+
+    sub('build', help='build static content from `data` directory')\
+        .arg('-s', '--serve', action='store_true', help='run server')
+
+    args = parser.parse_args()
+    if not hasattr(args, 'sub'):
+        parser.print_usage()
+
+    elif args.sub == 'run':
+        run_simple(
+            args.host, args.port, app,
+            use_reloader=True, use_debugger=True
+        )
+
+    elif args.sub == 'build':
+        build_dir = './var/data'
+        data.build('./data', build_dir)
+        if args.serve:
+            os.chdir(build_dir)
+            http.server.test(HandlerClass=http.server.SimpleHTTPRequestHandler)
 
 
 if __name__ == '__main__':
-    dispatch_commands([run, build])
+    process_args()
