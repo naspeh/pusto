a1c83d7e3eea7713d652a9e7f0de4801b650da7b
Grisha K <naspeh@gmail.com>
2013-09-22 14:03:11 +0300

    Squashed 'data/s/napokaz/src/' changes from d924078..6db63ff
    6db63ff Add support picasa albumid;
    33797ca Update escaping html in napokaz.js;
    4c9510c Fix photo description in napokaz.js;
    
    git-subtree-dir: data/s/napokaz/src
    git-subtree-split: 6db63ffba5dfd2a1772eb035f8a07a9042a2f725

--- napokaz.js
+++ napokaz.js
@@ -12,14 +12,20 @@
         // Picasa options
         picasaUser: 'naspeh',
         picasaAlbum: 'Naspeh',
+        picasaAlbumid: '',
         picasaFilter: '',
         picasaIgnore: ''
     };
     var picasa = {
         fetch: function(opts, success) {
+            var parts = ['user', opts.picasaUser];
+            if (opts.picasaAlbumid) {
+                parts = parts.concat(['albumid', opts.picasaAlbumid]);
+            } else {
+                parts = parts.concat(['album', opts.picasaAlbum]);
+            }
             $.ajax({
-                url: 'https://picasaweb.google.com/data/feed/api/' +
-                    ['user', opts.picasaUser, 'album', opts.picasaAlbum].join('/'),
+                url: 'https://picasaweb.google.com/data/feed/api/' + parts.join('/'),
                 data: {
                     kind: 'photo',
                     thumbsize: [opts.boxThumbsize, opts.frontThumbsize].join(',')
@@ -63,9 +69,9 @@
                         url: thumb2.attr('url'),
                         size: opts.frontThumbsizeInt
                     },
-                    'title': media.find('media\\:title').text(),
-                    'desc': media.find('media\\:description').text(),
-                    'tags': media.find('media\\:keywords').text()
+                    'title': escapeHtml(media.find('media\\:title').text()),
+                    'desc': escapeHtml(media.find('media\\:description').text()),
+                    'tags': escapeHtml(media.find('media\\:keywords').text())
                 };
                 if (picasa.checkTags(opts, item.tags)) {
                     items.push(item);
@@ -350,6 +356,22 @@
         o.picasaIgnore = picasa.parseTags(o.picasaIgnore);
         return o;
     }
+
+    // Taken from mustache.js
+    var entityMap = {
+        "&": "&amp;",
+        "<": "&lt;",
+        ">": "&gt;",
+        '"': '&quot;',
+        "'": '&#39;',
+        "/": '&#x2F;'
+    };
+    function escapeHtml(string) {
+        return String(string).replace(/[&<>"'\/]/g, function (s) {
+            return entityMap[s];
+        });
+    }
+
     // Taken from underscore.js with reformating and another delimiters.
     // JavaScript micro-templating, similar to John Resig's implementation.
     // Underscore templating handles arbitrary delimiters, preserves whitespace,
